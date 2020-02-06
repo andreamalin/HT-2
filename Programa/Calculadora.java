464a051ccd20c7e4936d43c2a8063437c98e7bfd
Andrea <andreaa.malin@gmail.com>
2020-02-05 21:19:54 -0600

    Se agregó defensiva

--- Programa/Calculadora.java
+++ Programa/Calculadora.java
@@ -23,7 +23,11 @@ public class Calculadora implements iCalculadora{
 	}
 
 	public int dividir(int operando2, int operando1){
-		return operando2/operando1;
+		if (operando1==0) {
+			return -1; //Error
+		} else {
+			return operando2/operando1;
+		}
 	}
 
 	public int multiplicar(int operando2, int operando1){
--- Programa/driver.java
+++ Programa/driver.java
@@ -24,12 +24,18 @@ public class driver {
 		iStack<Integer> stack = new Stack<Integer>(); 
 
 		//Se lee el txt
-		Scanner r = new Scanner(new File("datos.txt"));
-		while( r.hasNextLine()) {
-			txt += r.nextLine();
+		try {
+			Scanner r = new Scanner(new File("datos.txt")); 
+			while (r.hasNextLine()) {
+				txt += (r.nextLine()); //Mientras hayan lineas por leer se meten a la variable txt
+			}
+			r.close();	
+		} catch (Exception e) { //Se muestra la razon de error por la que no se encuentra el doc
+			e.printStackTrace();
 		}
+		
 		operacion = txt.split(" "); //Operacion creada a partir del txt
-		r.close();	
+		
 
 		//Se opera
 		for(int i = 0; i < operacion.length; i++){
@@ -38,29 +44,34 @@ public class driver {
 				stack.push(valor); //Si es entero se agrega
 
 			} catch (Exception e){
-				//Se jalan los dos últimos operandos del stack
-				operando2 = stack.pop();
-				operando1 = stack.pop();
-				
-				//Se busca el operador
-				switch(operacion[i]) {
-					case "+":
-				        valor = calculadora.sumar(operando2, operando1);
-                        break;
-					case "-":
-						valor = calculadora.restar(operando2, operando1);
-						break;
-					case "*":
-						valor = calculadora.multiplicar(operando2, operando1);
-						break;
-					case "/":
-						valor =  calculadora.dividir(operando2, operando1);
-						break;
-					default:
-						System.out.println("Error");
-						break;								
+				//No hay suficientes operandos
+				if (stack.size() > 2) {
+					//Se jalan los dos últimos operandos del stack
+					operando2 = stack.pop();
+					operando1 = stack.pop();
+					
+					//Se busca el operador
+					switch(operacion[i]) {
+						case "+":
+					        valor = calculadora.sumar(operando2, operando1);
+	                        break;
+						case "-":
+							valor = calculadora.restar(operando2, operando1);
+							break;
+						case "*":
+							valor = calculadora.multiplicar(operando2, operando1);
+							break;
+						case "/":
+							valor =  calculadora.dividir(operando2, operando1);
+							break;
+						default:
+							System.out.println("Error");
+							break;								
+					}
+					stack.push(valor);	
+				} else {
+					System.out.println("_______________________________________\n         NO HAY SUFICIENTES DATOS \n_______________________________________");
 				}
-				stack.push(valor);
 			}
 		}
 			
